25d9fba0b05d925a1de78d2e5b3f6865e8c0da8a
tao_ssh <1227602328@qq.com>
2021-09-17 13:49:58 +0800

    Update pipeline-param.groovy

--- Jenkinsfile/pipeline-param.groovy
+++ Jenkinsfile/pipeline-param.groovy
@@ -4,6 +4,7 @@ pipeline {
     
   parameters {
     string defaultValue: 'v1.0.0', description: 'version', name: 'version', trim: true
+    string defaultValue: 'demo-server', name: 'server name', trim: true
   }
   
     stages {
